dc25199c12e8da599dcb6529728d04e823be4a4b
mattigu <gulinski.mateusz1@gmail.com>
2026-01-04 13:25:57 +0100

    fix: swap to enumarete and remove channel identity from the message.

--- app/callbacks/Channel.cpp
+++ app/callbacks/Channel.cpp
@@ -10,15 +10,16 @@ void list(sub::channel::ListOptions const& options,
           teams::GraphServiceClient& client) {
     // TODO!
     // Channel and team is currently hardoded by id
-    auto channels = client.teams()
-                        .byId("948a9a8c-04f9-4e1c-9423-60b68f29b7dc")
-                        .channels()
-                        .get();
+    auto const channels = client.teams()
+                              .byId("948a9a8c-04f9-4e1c-9423-60b68f29b7dc")
+                              .channels()
+                              .get();
 
     std::println("List of channels:");
     if (channels) {
-        for (auto idx = 0; const auto& channel : channels.value()) {
-            std::println("{}. {}", ++idx, channel.display_name.value());
+        for (auto const [idx, channel] :
+             std::views::enumerate(channels.value())) {
+            std::println("{}. {}", idx, channel.display_name.value());
         }
     }
 }
--- app/callbacks/Team.cpp
+++ app/callbacks/Team.cpp
@@ -8,11 +8,11 @@ void add(sub::team::AddOptions const& options,
 
 void list(sub::team::ListOptions const& options,
           teams::GraphServiceClient& client) {
-    auto teams = client.teams().get();
+    auto const teams = client.teams().get();
     std::println("List of teams:");
     if (teams) {
-        for (auto idx = 0; const auto& team : teams.value()) {
-            std::println("{}. {}", ++idx, team.display_name.value());
+        for (auto const [idx, team] : std::views::enumerate(teams.value())) {
+            std::println("{}. {}", idx, team.display_name.value());
         }
     }
 }
