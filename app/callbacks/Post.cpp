d9dd1eddba8ae0433846d618652f2e895e014f78
mattigu <gulinski.mateusz1@gmail.com>
2026-01-04 13:25:57 +0100

    fix: swap to enumarete and remove channel identity from the message.

--- app/callbacks/Post.cpp
+++ app/callbacks/Post.cpp
@@ -7,11 +7,6 @@ void add(sub::post::AddOptions const& options,
     // Channel and team is currently hardcoded by id
 
     auto msg = teams::Message{
-        .channel_identity =
-            teams::priv::ChannelIdentity{
-                .channel_id =
-                    "19%3A3d5b1a7b3530488da59fd2df72ace3f8%40thread.tacv2",
-                .team_id = "948a9a8c-04f9-4e1c-9423-60b68f29b7dc"},
         .body = teams::priv::ItemBody{.content = options.text,
                                       .content_type = "text"}};
     auto result =
